cfd1bdc908b12973209b8201bf4e7ce001193940
Oktavian Mayer <oktavianmayer@icloud.com>
2026-03-27 11:41:38 +0100

    fixing up bootscript for first texts

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,3 +4,17 @@ echo "█▀▀▀▀▀▄   ▄▀▄   █▀▀▀▀▀▄   ▄▀▄   �
 █▄▄▄▄▄▀ ▄▀   ▀▄ █▄▄▄▄▄▀ ▄▀   ▀▄ █       █     █ █ ▀▄  █
 █       █▀▀▀▀▀█ █  ▀▄   █▀▀▀▀▀█ █   ▀▀█ █     █ █   ▀▄█
 █       █     █ █    ▀▄ █     █ ▀▄▄▄▄▄▀ ▀▄▄▄▄▄▀ █     █"
+
+echo "Installing all packages..."
+sudo pacman -S --noconfirm --needed - < packages/pacman.txt
+
+echo "Installing yay..."
+cd /tmp
+git clone https://aur.archlinux.org/yay.git
+cd yay
+makepkg -si
+
+echo "Installing all yay packages..."
+yay -S --noconfirm --needed - < packages/yay.txt
+
+echo "Done!"
